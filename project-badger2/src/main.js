d915706c80437d6fe80a07db774e0480bd640543
squonkbadger <tatianatassi19@gmail.com>
2019-08-20 16:44:32 +0300

    setup2

--- project-badger2/src/main.js
+++ project-badger2/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
